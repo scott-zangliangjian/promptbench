b19e6365bc67b417d59c4398531d9972171b6d18
Scott Zang <scott.zangliangjian@gmail.com>
2024-06-06 07:03:07 +0000

    minor changes

--- promptbench/models/__init__.py
+++ promptbench/models/__init__.py
@@ -5,32 +5,29 @@ from .models import *
 
 # A dictionary mapping of model architecture to its supported model names
 MODEL_LIST = {
-    T5Model: ['google/flan-t5-large'],
-    LlamaModel: ['llama2-7b', 'llama2-7b-chat', 'llama2-13b', 'llama2-13b-chat', 'llama2-70b', 'llama2-70b-chat',],
-    PhiModel: ['phi-1.5', 'phi-2'],
-    PaLMModel: ['palm'],
-    OpenAIModel: ['gpt-3.5-turbo', 'gpt-4', 'gpt-4-1106-preview', 'gpt-3.5-turbo-1106', 'gpt-4-0125-preview', 'gpt-3.5-turbo-0125', 'gpt-4-turbo', 'gpt-4o'],
-    VicunaModel: ['vicuna-7b', 'vicuna-13b', 'vicuna-13b-v1.3'],
-    UL2Model: ['google/flan-ul2'],
-    GeminiModel: ['gemini-pro'],
-    MistralModel: ['mistralai/Mistral-7B-v0.1', 'mistralai/Mistral-7B-Instruct-v0.1'],
-    MixtralModel: ['mistralai/Mixtral-8x7B-v0.1', 'mistralai/Mixtral-8x7B-Instruct-v0.1'],
-    YiModel: ['01-ai/Yi-6B', '01-ai/Yi-34B', '01-ai/Yi-6B-Chat', '01-ai/Yi-34B-Chat'],
-    BaichuanModel: ['baichuan-inc/Baichuan2-7B-Base', 'baichuan-inc/Baichuan2-13B-Base',
-                    'baichuan-inc/Baichuan2-7B-Chat', 'baichuan-inc/Baichuan2-13B-Chat'],
+    T5Model:       ['google/flan-t5-large'],
+    LlamaModel:    ['llama2-7b', 'llama2-7b-chat', 'llama2-13b', 'llama2-13b-chat', 'llama2-70b', 'llama2-70b-chat',],
+    PhiModel:      ['phi-1.5', 'phi-2'],
+    PaLMModel:     ['palm'],
+    OpenAIModel:   ['gpt-3.5-turbo', 'gpt-4', 'gpt-4-1106-preview', 'gpt-3.5-turbo-1106', 'gpt-4-0125-preview', 'gpt-3.5-turbo-0125', 'gpt-4-turbo', 'gpt-4o'],
+    VicunaModel:   ['vicuna-7b', 'vicuna-13b', 'vicuna-13b-v1.3'],
+    UL2Model:      ['google/flan-ul2'],
+    GeminiModel:   ['gemini-pro'],
+    MistralModel:  ['mistralai/Mistral-7B-v0.1', 'mistralai/Mistral-7B-Instruct-v0.1'],
+    MixtralModel:  ['mistralai/Mixtral-8x7B-v0.1', 'mistralai/Mixtral-8x7B-Instruct-v0.1'],
+    YiModel:       ['01-ai/Yi-6B', '01-ai/Yi-34B', '01-ai/Yi-6B-Chat', '01-ai/Yi-34B-Chat'],
+    BaichuanModel: ['baichuan-inc/Baichuan2-7B-Base', 'baichuan-inc/Baichuan2-13B-Base', 'baichuan-inc/Baichuan2-7B-Chat', 'baichuan-inc/Baichuan2-13B-Chat'],
 }
 MODEL_LIST_VLM = {
-    BLIP2Model: ['Salesforce/blip2-opt-2.7b', 'Salesforce/blip2-opt-6.7b',
-                 'Salesforce/blip2-flan-t5-xl', 'Salesforce/blip2-flan-t5-xxl'],
-    LLaVAModel: ['llava-hf/llava-1.5-7b-hf', 'llava-hf/llava-1.5-13b-hf'],
-    GeminiVisionModel: ['gemini-pro-vision'],
-    OpenAIVisionModel: ['gpt-4-vision-preview'],
-    QwenVLModel: ['Qwen/Qwen-VL', 'Qwen/Qwen-VL-Chat',
-                  'qwen-vl-plus', 'qwen-vl-max'],
+    BLIP2Model:          ['Salesforce/blip2-opt-2.7b', 'Salesforce/blip2-opt-6.7b', 'Salesforce/blip2-flan-t5-xl', 'Salesforce/blip2-flan-t5-xxl'],
+    LLaVAModel:          ['llava-hf/llava-1.5-7b-hf', 'llava-hf/llava-1.5-13b-hf'],
+    GeminiVisionModel:   ['gemini-pro-vision'],
+    OpenAIVisionModel:   ['gpt-4-vision-preview'],
+    QwenVLModel:         ['Qwen/Qwen-VL', 'Qwen/Qwen-VL-Chat', 'qwen-vl-plus', 'qwen-vl-max'],
     InternLMVisionModel: ['internlm/internlm-xcomposer2-vl-7b'],
 }
 
-SUPPORTED_MODELS = [model for model_class in MODEL_LIST.keys() for model in MODEL_LIST[model_class]]
+SUPPORTED_MODELS     = [model for model_class in MODEL_LIST.keys()     for model in MODEL_LIST[model_class]]
 SUPPORTED_MODELS_VLM = [model for model_class in MODEL_LIST_VLM.keys() for model in MODEL_LIST_VLM[model_class]]
 
 
@@ -92,14 +89,10 @@ class LLMModel(object):
         # Get the model class based on the model name and instantiate it
         model_class = model_mapping.get(self.model_name)
         if model_class:
-            if model_class == LlamaModel or model_class == VicunaModel:
-                return model_class(self.model_name, max_new_tokens, temperature, device, dtype, system_prompt, model_dir)
-            elif model_class in [OpenAIModel]:
-                return model_class(self.model_name, max_new_tokens, temperature, system_prompt, api_key)
-            elif model_class in [PaLMModel, GeminiModel]:
-                return model_class(self.model_name, max_new_tokens, temperature, api_key)
-            else:
-                return model_class(self.model_name, max_new_tokens, temperature, device, dtype)
+            if   model_class == LlamaModel or model_class == VicunaModel: return model_class(self.model_name, max_new_tokens, temperature, device, dtype, system_prompt, model_dir)
+            elif model_class in [OpenAIModel]:                            return model_class(self.model_name, max_new_tokens, temperature, system_prompt, api_key)
+            elif model_class in [PaLMModel, GeminiModel]:                 return model_class(self.model_name, max_new_tokens, temperature, api_key)
+            else:                                                         return model_class(self.model_name, max_new_tokens, temperature, device, dtype)
         else:
             raise ValueError("The model is not supported!")
 
